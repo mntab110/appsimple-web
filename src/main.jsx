7ced00854347b56c181f23e0f88119fb066f3842
mntab110 <matt@tabrizi.me>
2026-05-24 14:38:50 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomePage from "./app/page";
+import PrivacyPage from "./app/privacy/page";
+
+function App() {
+  const path = window.location.pathname;
+
+  if (path === "/privacy") {
+    return (
+      <div className="min-h-screen flex flex-col">
+        <PrivacyPage />
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <HomePage />
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
