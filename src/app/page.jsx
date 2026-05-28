7641c8d952c7b9aa7962bac629b591fce2b69cfa
mntab110 <214683976+mntab110@users.noreply.github.com>
2026-05-28 11:54:48 -0400

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,7 +27,7 @@ export default function HomePage() {
         "https://appsimple.com/images/forknotes-onboarding-1.jpg",
         "https://appsimple.com/images/forknotes-onboarding-2.png",
         "https://appsimple.com/images/forknotes-onboarding-3.png",
-        "https://appsimple.com/images/forknotes-onboarding-3.png",
+        "https://appsimple.com/images/forknotes-onboarding-4.png",
       ],
       type: "Mobile App",
     },
