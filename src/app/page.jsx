b310dc85155404aeb8ca745ec39774db198e208b
mntab110 <214683976+mntab110@users.noreply.github.com>
2026-05-28 10:47:48 -0400

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -24,8 +24,8 @@ export default function HomePage() {
       image:
         "https://appsimple.com/images/forknotes-postcard.jpg",
       onboarding: [
-        "https://appsimple.com/images/forknotes-onboarding-1.png",
-        "https://appsimple.com/images/forknotes-onboarding-2.png",
+        "https://appsimple.com/images/forknotes-onboarding-1",
+        "https://appsimple.com/images/forknotes-onboarding-2.jpg",
         "https://appsimple.com/images/forknotes-onboarding-3.png",
         "https://appsimple.com/images/forknotes-onboarding-4.png",
       ],
